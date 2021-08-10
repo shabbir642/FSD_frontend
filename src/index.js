6922d14d0281f36f9ea7424c4241eacfd9547ae0
shabbir642 <87113084+shabbir642@users.noreply.github.com>
2021-08-10 09:55:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,34 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
+import { Provider } from 'react-redux';
+import configureStore from './Store';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <React.StrictMode>
+    <Provider store={configureStore()}>
+       <App />
+    </Provider>
+    </React.StrictMode>,
     document.getElementById("root")
 );
-
-// serviceWorker.unregister();
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import './index.css';
-// import 'bootstrap/dist/css/bootstrap.css';
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
