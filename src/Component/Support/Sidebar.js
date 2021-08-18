c64ab0021fb283fd39928d2e5776f85c54014433
shabbir642 <87113084+shabbir642@users.noreply.github.com>
2021-08-18 12:29:58 +0530

    Update Sidebar.js

--- src/Component/Support/Sidebar.js
+++ src/Component/Support/Sidebar.js
@@ -2,11 +2,6 @@ import React from 'react';
 import './style.css';
 import { Link } from "react-router-dom";
 function Sideheader() {
-  //    logout(e) {
-  //   e.preventDefault()
-  //   localStorage.clear();
-  //   this.props.history.push(`/`);
-  // }
     return (
         <>
         <div className="sidenav">
@@ -14,7 +9,6 @@ function Sideheader() {
            <Link to="/Dashboard">Dashboard</Link>
            <Link to="/Tasklist">Task</Link>
            <Link to="/Userlist">Users</Link>
-           { localStorage.getItem("token") && JSON.parse(localStorage.getItem("user")).admin ? <Link to="/Alltask">Alltask</Link> : " "}
            <Link to="/Logout">Logout</Link>
 
         </div>
